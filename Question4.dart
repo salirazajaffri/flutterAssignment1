139b7584b50b5c0ac2bd5dd4b87092a7fd060054
ali12kafour <alirazajaffri1105@gmail.com>
2023-06-16 21:53:08 +0500

    add Question4 file and code

---
+++ Question4.dart
@@ -0,0 +1,12 @@
+void main() {
+  //-----------Q4-----------------
+  int year = 123;
+
+  if (year % 4 == 0) {
+    print("Leap Year");
+  } else if (year % 400 == 0) {
+    print("Leap Year");
+  } else {
+    print("Not a Leap Year");
+  }
+}
