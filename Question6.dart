25481e65c9f83f491dc70d1630047985c3a49cf7
ali12kafour <alirazajaffri1105@gmail.com>
2023-06-16 21:58:15 +0500

    add Question6 file and code

---
+++ Question6.dart
@@ -0,0 +1,18 @@
+void main() {
+  //-----------Q6-----------------
+  String alphabet = 'E';
+  if (alphabet == 'a' ||
+      alphabet == 'e' ||
+      alphabet == 'i' ||
+      alphabet == 'o' ||
+      alphabet == 'u' ||
+      alphabet == 'A' ||
+      alphabet == 'E' ||
+      alphabet == 'I' ||
+      alphabet == 'O' ||
+      alphabet == 'U') {
+    print("Vowel");
+  } else {
+    print("Consonant");
+  }
+}
